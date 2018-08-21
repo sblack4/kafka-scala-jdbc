6baa93c6732edc0de49b43d93b4c5720b812e339
sblack4 <genseb7@gmail.com>
2018-08-21 11:21:44 -0400

    went to single-file app for debugging

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,14 +5,19 @@ import org.apache.spark.sql._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 
+import java.sql._
+import java.security.Security
+import oracle.jdbc.pool.OracleDataSource
+import oracle.security.pki.OraclePKIProvider
+import org.apache.spark.SparkFiles
+import org.apache.spark.sql._
+import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
 
 
 object Main {
-    def main(args: Array[String]) = {
+    def main(args: scala.Array[String]) = {
 
-        System.setProperty("oracle.jdbc.Trace", "true")
-        System.setProperty("oracle.jdbc.level", "ALL")
-
+        @transient lazy val log = org.apache.log4j.LogManager.getLogger("myLogger")
         
         val brokers = args(0)
         val topic = args(1)
@@ -21,66 +26,85 @@ object Main {
         val connection = args(4)
         val filePath = args(5)
 
-        println("--- stuff ---")
-        println(brokers)
-        println(topic)
-        println(user)
-        println(connection)
-
-        val query = """
-            insert into tickets values ('asdfgafsd','asfdafsd','asdffasd','asffasd')
-            """
-
-
         val spark = SparkSession
             .builder()
             .appName("Stream to ADWC")
             .config("master", "yarn")
             .getOrCreate()
 
-        database.setOds(spark, filePath, connection, user, password)
-        database.write(query, connection, user, password)
 
-        import spark.implicits._
-        val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
+        // DB TEST 
 
-        val topicsSet = topic.split(",").toSet
-        val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-        val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+        // get local directory on executor
+        val tnsnames = SparkFiles.get("tnsnames.ora")
 
-        messages.print()
+        val localpath = SparkFiles.getRootDirectory()
 
-         messages.foreachRDD(rdd => {
-            var df = spark.read.json(rdd.map(x => x._2))
+        log.warn("SparkFiles.get return type: " + tnsnames.getClass.getSimpleName)
+
+        val fullConn = connection + localpath
+
+        log.warn("ADWC FULL CONNECTION STRING: " + fullConn)
+
+        val ods = new OracleDataSource()
+        ods.setURL(fullConn)
+        ods.setUser(user)
+        ods.setPassword(password)
+        log.info("Database Connection done")
+
+         val query = """
+            insert into tickets values ('asdfgafsd','asfdafsd','asdffasd','asffasd')
+            """       
+
+        val conn = ods.getConnection()
+        val stmt = conn.createStatement()
+
+        val rset = stmt.executeQuery(query)
+
+        rset.close()
+        stmt.close()
+        conn.close()
+
+        // import spark.implicits._
+        // val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
+
+        // val topicsSet = topic.split(",").toSet
+        // val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+        // val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+
+        // messages.print()
+
+        //  messages.foreachRDD(rdd => {
+        //     var df = spark.read.json(rdd.map(x => x._2))
         
-            // try {
-                val ds = df.as[classes.Ticket]
-                ds.show()
-        val tickets = df.select(
-            "id",
-            "date",
-            "category",
-            "comment"
-        )
+        //     // try {
+        //         val ds = df.as[classes.Ticket]
+        //         ds.show()
+        // val tickets = df.select(
+        //     "id",
+        //     "date",
+        //     "category",
+        //     "comment"
+        // )
 
-        tickets.foreach(row => {
-            val query = s"""
-            insert into tickets values ($row.id, $row.date, $row.category, $row.comment)
-            """
+        // tickets.foreach(row => {
+        //     val query = s"""
+        //     insert into tickets values ($row.id, $row.date, $row.category, $row.comment)
+        //     """
 
-            database.write(query, connection, user, password)
+        //     database.write(query, connection, user, password)
 
-        })
-                // utils.handleJson(ds)
-            // } catch {
-                // case e: Exception =>
-                    // println(e)
-            // }
-        })
+        // })
+        //         // utils.handleJson(ds)
+        //     // } catch {
+        //         // case e: Exception =>
+        //             // println(e)
+        //     // }
+        // })
 
-        println("Starting Streaming Context")
-        ssc.start()
-        ssc.awaitTermination()
+        // println("Starting Streaming Context")
+        // ssc.start()
+        // ssc.awaitTermination()
 
     }
 }
